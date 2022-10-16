7280d0ce12d7528d3fca047cad6dd92b20edb8bd
davifdepaula <davi.ferreira100@gmail.com>
2022-10-16 04:32:26 -0300

    adição do botão finalizar

--- script.js
+++ script.js
@@ -1,16 +1,31 @@
-let total = 0
+let total = 0, food="", dessert = "", drink=""
 function selection(element) {
     if (element.parentNode.classList[0] == "mainCourse"){
         makeSelection("mainCourse", element)
+        food = element.querySelector(".foodName")
+        food = food.innerHTML
     }
 
     else if (element.parentNode.classList[0] == "drink"){
         makeSelection("drink", element)
+        drink = element.querySelector(".foodName")
+        drink = drink.innerHTML
     }
 
     else if (element.parentNode.classList[0] == "dessert"){
         makeSelection("dessert", element)
+        dessert = element.querySelector(".foodName")
+        dessert = dessert.innerHTML
     } 
+
+    if(document.querySelector(`.mainCourse .select`) !== null &&
+    document.querySelector(`.drink .select`) !== null &&
+    document.querySelector(`.dessert .select`) !== null){
+        const parentClass = document.querySelector(".footerContainer")
+        parentClass.classList.add("footerContainerBackground")
+        const flag = document.querySelector(".footerText")
+        flag.innerHTML = "fechar pedido"
+    }
 };
 
 function makeSelection(parent, element) {
@@ -28,8 +43,17 @@ function makeSelection(parent, element) {
 
 };
 
-function getTotal(){
-    console.log(total);
+
+function getTotal(element){
+    if (element.classList.contains("footerContainerBackground")){
+        https://wa.me/5511968374030?text="fala"
+        console.log(food)
+        console.log(drink)
+        console.log(dessert)
+        console.log(total)
+    }
+
 };
+
 
 
